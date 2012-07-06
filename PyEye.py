e411656b5632323b3ee35bb5078b06b051123e76
bev-a-tron <beverly.a.lau@gmail.com>
2012-07-06 18:13:05 -0400

    Replicating pattern

--- PyEye.py
+++ PyEye.py
@@ -9,17 +9,44 @@
 6. Copy shape, move width of noise block to the left.
 """
 
-def main(dim=(600,250)):
+import numpy
+from PIL import Image
+
+def main(dim=(250,600)): # strange how it makes you put it in y,x
     center=(dim[0]/2,dim[1]/2)
     num_blocks = 4
 
     img = CreateCanvas(dim=dim)
+
+# TODO: should we just start with the noise block and replicate it out?
+#       maybe we don't neeeed a CreateCanvas...
+
+# TODO: ATM, canvas size has to be a multiple of block size.
+
     noise_block = CreateNoiseBlock(dim=dim,num_blocks=num_blocks)
-    img = CopyNoiseBlock(img,noise_block)
-    shape = GetShape(img,mode='circle',size=140,position=center)
-    processed_shape = MoveShape(shape,depth=10,,position=center)
-    neg_shape = MoveShape(shape,depth=-dim[0]/num_blocks)
-    new_img = SumImages(img,processed_shape,neg_shape)
+    img = CopyNoiseBlock(img,noise_block,num_blocks)
+   # shape = GetShape(img,mode='circle',size=140,position=center)
+   # processed_shape = MoveShape(shape,depth=10,,position=center)
+   # neg_shape = MoveShape(shape,depth=-dim[0]/num_blocks)
+   # new_img = SumImages(img,processed_shape,neg_shape)
 
 def CreateCanvas(dim):
-    
+    a=numpy.zeros([dim[0],dim[1],4],dtype='uint8')
+    return a
+
+def CreateNoiseBlock(dim, num_blocks):
+    img = Image.open('pattern.png')
+    img = numpy.array(img)
+    return img
+
+def CopyNoiseBlock(img, noise_block, num_blocks): # maybe call this ReplicateNoiseBlock ?
+    final = img.copy()
+    # final = img
+    for i in range(num_blocks):
+        size = 150
+        start = i * size
+        final[:,start:size+start,:]=noise_block
+    return final
+
+if __name__=="__main__":
+    main()
